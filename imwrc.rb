f6f557058d33a92d2240ea04dea518d9bdceb16c
Philip (flip) Kromer <flip@infochimps.org>
2008-08-25 21:41:15 -0500

    for InfiniteMonkeywrench

---
+++ imwrc.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+module IMW
+
+  DEFAULT_DATABASE_CONNECTION_PARAMS = {
+    :protocol => :mysql,
+    :username => 'imw',
+    :password => 'local_pass_14_312',
+    :hostname => 'localhost'
+  }
+
+end
