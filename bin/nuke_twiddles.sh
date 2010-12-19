485e375953e774e8a52a41f36f016b889534bb42
Philip (flip) Kromer <flip@infochimps.org>
2010-12-19 12:30:04 -0600

    a few personal utilities

--- bin/nuke_twiddles.sh
+++ bin/nuke_twiddles.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 timestamp=`date "+%Y%m%d%H%M%S"`
 bkupdir=/tmp/twiddles/$timestamp
-echo "Ditching removed files into ${bkupdir}"
+if [[ "$1" =~ ^--patt=(.*) ]] ; then
+  patt=${BASH_REMATCH[1]}
+  shift
+else
+  patt='*~'
+fi
+echo "Ditching files that match $patt into ${bkupdir}"
 mkdir -p $bkupdir
-find ${@-.} -name '*~' -print -exec mv -b {} $bkupdir/ \;
+find "${@-.}" -name "$patt" -print -exec mv -b {} $bkupdir/ \;
