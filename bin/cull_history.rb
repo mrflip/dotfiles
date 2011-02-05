eef652eab188e82319331b29ba607ddb42e13f8d
Philip (flip) Kromer <flip@infochimps.org>
2011-02-05 16:43:39 -0600

    fixed history config to not be as duplicative

--- bin/cull_history.rb
+++ bin/cull_history.rb
@@ -5,15 +5,17 @@ require 'set'
 HIST_DIR = File.dirname( ENV['HISTFILE'] )
 TMP_DIR = "/tmp/cull_history-#{Time.now.strftime("%Y%m%d%h%m%s")}-#{$$}"
 FileUtils.mkdir_p TMP_DIR
-$stderr.puts "Placing new history files in #{TMP_DIR}..."
+$stderr.puts "Placing old history files in #{TMP_DIR}..."
 
 # Track each unique line
 lines = Set.new
 
-Dir[HIST_DIR+'/*'].sort.each do |hist_file|
+Dir[HIST_DIR+'/hist-*'].sort.each do |hist_file|
   uniq_lines = 0 ; total_lines = 0
-  File.open(File.join(TMP_DIR, File.basename(hist_file)), 'w') do |out_file|
-    File.open(hist_file).each do |line|
+  orig_file = File.join(TMP_DIR, File.basename(hist_file))
+  FileUtils.cp hist_file, orig_file
+  File.open(hist_file, 'w') do |out_file|
+    File.open(orig_file).each do |line|
       total_lines += 1
       next if lines.include?(line) ; uniq_lines += 1
       lines    << line
